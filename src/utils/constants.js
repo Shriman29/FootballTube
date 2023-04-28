6cf2d2873e3ab6ba8e2cfc34966e1aea7f530623
Shriman29 <umashri2002.uu@gmail.com>
2023-04-28 21:03:48 +0530

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://cdn-icons-png.flaticon.com/128/4049/4049083.png';
+
+export const categories = [
+  { name: 'New Football', icon: <HomeIcon />, },
+  { name: 'Football Highlights', icon: <CodeIcon />, },
+  { name: 'Football Live', icon: <CodeIcon />, },
+  { name: 'World Cup', icon: <CodeIcon />, },
+  { name: 'UEFA Champions League', icon: <CodeIcon />, },
+  { name: 'UEFA Europa League', icon: <DeveloperModeIcon />, },
+  { name: 'English FA Cup', icon: <DeveloperModeIcon />, },
+  { name: 'UEFA EURO', icon: <MusicNoteIcon /> },
+  { name: 'Copa America', icon: <SchoolIcon />, },
+  { name: 'AFC Champions League', icon: <GraphicEqIcon />, },
+  { name: 'La Liga', icon: <OndemandVideoIcon />, },
+  { name: 'Premier League', icon: <SportsEsportsIcon />, },
+  { name: 'Bundesliga', icon: <SportsEsportsIcon />, },
+  { name: 'Ligue 1', icon: <LiveTvIcon />, },
+  { name: 'Serie A', icon: <FitnessCenterIcon />, },
+  { name: 'Primiera Liga', icon: <CheckroomIcon />, },
+  { name: 'Eredivisie', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Major League Soccer', icon: <TheaterComedyIcon />, },
+  { name: 'Indian Super League', icon: <FitnessCenterIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
